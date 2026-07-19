8fd55dbc314c3d3a6d07a039b67a3c1da8519841
rahdeva <wirandanaputra@gmail.com>
2026-07-19 09:39:37 +0700

    Initial Commit

---
+++ TwistSpeak/TwistSpeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwistSpeakApp.swift
+//  TwistSpeak
+//
+//  Created by rahdeva on 19/07/26.
+//
+
+import SwiftUI
+
+@main
+struct TwistSpeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
